d31a6a40be16ecf8558bbe7042bfe35a570d406b
Sina Schnebelt <sina-schnebelt@web.de>
2020-12-11 11:15:36 +0100

    Update values.js

--- values.js
+++ values.js
@@ -1,5 +1,5 @@
 
-
+//wir könnten die Daten entweder in json in eine funktion schreiben oder über die API abrufen(?!) 
 window.onload = function(){
 var http =  new XMLHttpRequest();
 http.open("GET", "https://opendata.arcgis.com/datasets/ef4b445a53c1406892257fe63129a8ea_0.geojson");
