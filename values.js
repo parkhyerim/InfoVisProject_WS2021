a8fc340900b392d0aeb3cc8dfc872ffb4075b07a
Sina Schnebelt <sina-schnebelt@web.de>
2020-12-11 11:11:30 +0100

    Grundgerüst

--- values.js
+++ values.js
@@ -1,6 +1,7 @@
-//Alle Daten in eine Funktion damit diese abrufbar ist
-filterData([
-    { //Json Daten
-    
-    }
-])
+
+
+window.onload = function(){
+var http =  new XMLHttpRequest();
+http.open("GET", "https://opendata.arcgis.com/datasets/ef4b445a53c1406892257fe63129a8ea_0.geojson");
+http.send(); 
+};
