c836b196d2773ef3fed603abb07edaf2bbe5f93a
Hyerim Park <hallohyerim@gmail.com>
2021-01-13 19:38:33 +0100

    Fix minor problems (overlayed texts)

--- src/scripts/treemapMobilityView.js
+++ src/scripts/treemapMobilityView.js
@@ -1,57 +1,77 @@
+
+let myMonat = "";
+
 // temporary: the entry point for the regions selection ('chekbox' in index.html)
 const checkboxes = document.getElementsByClassName('checkbox')
-let countCheck = 0; // number of checked regions
+let counterRegion = 0; // number of checked regions
 for (let checkbox of checkboxes){
   checkbox.addEventListener('change', function() {
       if(this.checked){
-          displaymobilitydata(checkbox.name, "10")  // params: region, month
-          countCheck++;
-          console.log(countCheck)      
+           displaymobilitydata(checkbox.name, "03")
+        //  displaymobilitydata(checkbox.name, myMonat)  // params: region, month
+        // else
+        
+          // displaymobilitydata("Berlin", "01") 
+          counterRegion++; 
       }else{
-          countCheck--;
-        // console.log(countCheck)
+          counterRegion--;
       }
   }) 
 }
 
+
+
+// const monatcheckboxes = document.getElementsByClassName('monatcheckbox')
+// for (let checkbox of monatcheckboxes){
+//   checkbox.addEventListener('change', function() {
+//       if(this.checked){
+//          // displaymobilitydata(checkbox.id, "jan")  // params: region, month
+//          // displaymobilitydata("Berlin", "01") 
+//           myMonat = checkbox.id
+//          console.log(myMonat)      
+//       }else{
+//           myMonat = ""
+//         console.log(myMonat) 
+//       }
+//   }) 
+// }
+
+
+
 function displaymobilitydata(regionParam, monthParam){
 
-    let temp = [];
     let mobilityData = [];
-
+    var temp = [];
     d3.csv('../src/data/applemobilitytrends.csv').then(function(data){
-        data.forEach(element => temp.push(element));
-        temp.filter(function(element){
-            if (element.country == "Germany" && element["sub-region"] == ""){
-                mobilityData.push(element);   
-               // mobilityData.push({jan:"", feb:""})
-              //mobilityData.push({country: element.country, region: element.region, transportation_type: element.transportation_type, jan:"", feb:""});   
-                //console.log(mobilityData)                 
-            }
-        })
-
-        console.log(mobilityData)  
-              
-        // data.forEach(function(element) {
-        //     if (element.country == "Germany" && element["sub-region"] == "") {
-        //         mobilityData.push(element);    
-        //         console.log(mobilityData)    
+        // data.filter(function(element){
+        //     if (element.country == "Germany" && element["sub-region"] == ""){
+        //         mobilityData.push(element);   
+        //      // mobilityData.push({country: element.country, region: element.region, transportation_type: element.transportation_type, jan:"", feb:"", mar:""});   
+        //         //console.log(mobilityData)                 
         //     }
-        // });
+        // })
 
-        // console.log(mobilityData)   
+        data.forEach(element => temp.push(element));
+        temp.forEach(function(element) {
+            if (element.country == "Germany" && element["sub-region"] == "" &&  element["region"]==regionParam) {
+                mobilityData.push(element);    
+                // console.log(mobilityData)    
+            }
+        });
+
+        console.log(mobilityData)   
 
         // Create a nested array 
         let nestedData = d3.group(mobilityData, d => d.region, d => d.transportation_type)
-        console.log("a nested array")        
-        console.log(nestedData)
+        // console.log("a nested array")        
+        // console.log(nestedData)
 
         let dataByRegion = nestedData.get(regionParam)
-        console.log("data by region")
-        console.log(dataByRegion)
+        // console.log("data by region")
+        // console.log(dataByRegion)
         let dataByDriving = dataByRegion.get("driving")
-        console.log("data by driving")
-        console.log(dataByDriving)
+        // console.log("data by driving")
+        // console.log(dataByDriving)
         let dataByWalking = dataByRegion.get("walking")
         let dataByTransit = dataByRegion.get("transit")
         
@@ -59,23 +79,134 @@ function displaymobilitydata(regionParam, monthParam){
         if(dataByDriving) CalculateMonthlyAverage(dataByDriving)
         if(dataByWalking) CalculateMonthlyAverage(dataByWalking)
         if(dataByTransit) CalculateMonthlyAverage(dataByTransit)
-        
-      
-        //generate TreeChart from the provided Dateset
-       // console.log(mobilityData)    
-      //  let mobilityDataPerMonth = d3.ascending(mobilityData);
-        /* console.log(mobilityDataPerMonth)   
-        console.log(mobilityDataPerMonth[1][5]) 
-        console.log(mobilityDataPerMonth[0][5]) 
-        console.log(mobilityDataPerMonth[2][5])  */
-       // generateHierarchy(mobilityDataPerMonth)
-      // console.log(mobilityData)
-       createTreeChart(mobilityData, monthParam)
+ 
+        createTreemapData(mobilityData, monthParam)
+        console.log("test")
+        console.log(mobilityData)
     });
+   
 };
 
 // temporary: display the treemap for default params: Bavaria, January (without checkbox selection)
- displaymobilitydata("Bavaria", "01");
+// displaymobilitydata("Bavaria", "01");
+
+function createTreemapData(data, month){
+    let myData = data;
+    console.log("crete0")
+    console.log(myData)
+
+    data.forEach(function(element) {
+      
+                // mobilityData.push(element);    
+                // console.log(mobilityData)    
+                // mobilityData.push({country: element.country, region: element.region, transportation_type: element.transportation_type, jan:"", feb:"", mar:""});  
+
+        });
+
+
+//Group the data by "Germany", so our tree has a root node
+let groupedData = data.reduce((k, v)=> {
+    k[v.country] = [...k[v.country] || [], v];
+    return k;
+    }, {}); 
+
+//Group the data by country and by region, so our tree has a root node
+// let groupedData = data.reduce((c, v) => {
+//     c[v.country] = c[v.country] || {};                         //Init if country property does not exist
+//     c[v.country][v.region] = c[v.country][v.region] || {};   //Init if region property does not exist
+//     return c;
+//   }, {});
+
+// let hierachyData = d3.group(groupedData, d => d.region, d => d.transportation_type)
+// console.log(hierachyData)
+
+
+//Transform the data grouped by "Germany" into a hiearchy by usind d3.js hierachy (first param is root, second param is child nodes)
+let hgroup = d3.hierarchy(groupedData, function(d){
+                      return d.Germany})
+    .sum((d) => {return d[month]});
+
+createTreeChart(hgroup, month);
+
+};
+
+
+function createTreeChart(hgroup, month){
+
+    // Old treemap gets removed
+    d3.select("#treemapRegion01").select("svg").remove();
+
+    var margin = {top: 10, right: 10, bottom: 10, left: 10};
+    var width = 400;
+    var height = 400;
+
+    var svg = d3.select("#treemapRegion01")
+        .append("svg")
+        .attr("width", width + 20)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+    // Then d3.treemap computes the position of each element of the hierarchy
+    // The coordinates are added to the root object above
+   const treemap= d3.treemap()
+       .size([width, height])
+       .padding(2)
+       .paddingInner(2)
+       (hgroup)
+
+
+    // color options
+    var color= d3.scale.linear().domain([60, 300]).range(["orange", "indianred"]);
+    var color1 = d3.scale.category10();
+    var color2 = d3.scale.category20();         
+    var color3 = d3.scale.category20b();  
+    var color4 = d3.scale.category20c();  
+
+    // Add a opacity scale
+    var opacity =d3.scaleLinear()
+                .domain([90, 150])
+                .range([.6,1])
+
+    // use this information to add rectangles:
+    svg
+        .selectAll("rect")
+        .data(treemap.leaves())
+        .enter()
+        .append("rect")
+        .attr("id", (d) =>{return d.id;})
+        .attr('x', function (d) { return d.x0; })
+        .attr('y', function (d) { return d.y0})
+        .attr('width', function (d) { return d.x1 - d.x0; })
+        .attr('height', function (d) { return d.y1 - d.y0})
+        .style("fill", function(d) {
+            return color2(d.data[month]);})
+            .style("opacity", function(d) {
+                return opacity(d.data[month])
+            });
+   
+        
+    // and to add the text labels
+    svg
+    .selectAll("text")
+    .data(treemap.leaves())
+    .enter()
+    .append("text")
+    .attr("x", function(d){ return d.x0+20})    // +10 to adjust position (more right)
+    .attr("y", function(d){ return d.y0+30})    // +20 to adjust position (lower)
+    .text(function(d){ 
+        // Temporal: kurze Syntax und bessere Images
+        if(d.data.transportation_type === "driving"){
+        return "🚘 " + d.data.transportation_type +" "+ d.data[month]+"%"; }
+     else if(d.data.transportation_type === "walking"){
+        return "🚶‍♀️ " + d.data.transportation_type +" "+ d.data[month]+"%";
+    } else if(d.data.transportation_type === "transit") {
+        return "🚌 "+ d.data.transportation_type + " "+ d.data[month]+"%";
+    }})
+    .attr("font-size", "14px")
+    .attr("fill", "white")
+        }
 
 
 // Calculate the monthly average separately for the means of transport 
@@ -188,138 +319,5 @@ function CalculateMonthlyAverage(data){
                 }
 
             });
-        }}}
-
-
-function createTreeChart(data, month){
-
-    //in case new treemap shall be loaded, the one before gets removed
-    d3.select("#treemapMobilitywrapper").select("svg").remove();
-    d3.select("#treemapMobilitywrapper2").select("svg").remove();
-
-
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = 450 - margin.left - margin.right,
-        height = 450 - margin.top - margin.bottom;
-
-    var svg = d3.select("#treemapMobilitywrapper")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-       
-        var svg2 = d3.select("#treemapMobilitywrapper2")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    //Group the data by "Germany", so our tree has a root node
-    let groupedData = data.reduce((k, v)=> {
-        k[v.country] = [...k[v.country] || [], v];
-        return k;
-        }, {}); 
-
-       // console.log(groupedData)
-
-    // let hierachyData = d3.group(groupedData, d => d.region, d => d.transportation_type)
-    // console.log(hierachyData)
-    //Group the data by country and by region, so our tree has a root node
-        // let groupedData = data.reduce((c, v) => {
-        //     c[v.country] = c[v.country] || {};                         //Init if country property does not exist
-        //     c[v.country][v.region] = c[v.country][v.region] || {};   //Init if region property does not exist
-        //     return c;
-        //   }, {});
-        //   console.log("groupData: " + groupedData);
-
-
-    //Transform the data grouped by "Germany" into a hiearchy by usind d3.js hierachy (first param is root, second param is child nodes)
-    var hgroup = d3.hierarchy(groupedData, function(d){
-                                return d.Germany}
-                                )
-        .sum((d) => {return d[month]});
-
-      //  console.log(hgroup)
-
-    // Then d3.treemap computes the position of each element of the hierarchy
-    // The coordinates are added to the root object above
-   const treemap= d3.treemap()
-       .size([width, height])
-       .padding(4)
-       .paddingInner(3)
-      (hgroup)
-
-
-    // Determine the color of each field
-    // Explanation from https://stackoverflow.com/questions/42546344/how-to-apply-specific-colors-to-d3-js-map-based-on-data-values?rq=1
-    var color= d3.scale.linear()
-                .domain([80, 150])
-                .range(["orange", "indianred"]);
-
-    // Add a opacity scale
-    var opacity =d3.scaleLinear()
-                .domain([50, 120])
-                .range([.4,1])
-
-    // use this information to add rectangles:
-    svg
-        .selectAll("rect")
-        .data(treemap.leaves())
-        .enter()
-        .append("rect")
-        .attr("id", (d) =>{return d.id;})
-        .attr('x', function (d) { return d.x0; })
-        .attr('y', function (d) { return d.y0})
-        .attr('width', function (d) { return d.x1 - d.x0; })
-        .attr('height', function (d) { return d.y1 - d.y0})
-        .style("fill", function(d) {
-            return color(d.data[month]);})
-            .style("opacity", function(d) {
-                return opacity(d.data[month])
-            });
-   
-        
-    // and to add the text labels
-    svg
-    .selectAll("text")
-    .data(treemap.leaves())
-    .enter()
-    .append("text")
-    .attr("x", function(d){ return d.x0+20})    // +10 to adjust position (more right)
-    .attr("y", function(d){ return d.y0+30})    // +20 to adjust position (lower)
-    .text(function(d){ if(d.data.transportation_type != "" && d.data.transportation_type != undefined && d.data.transportation_type != null) return d.data.transportation_type +""+ d.data[month]+"%"})
-    .attr("font-size", "18px")
-    .attr("fill", "white")
-
-
-
-    svg2
-    .selectAll("rect")
-    .data(treemap.leaves())
-    .enter()
-    .append("rect")
-    .attr("id", (d) =>{return d.id;})
-    .attr('x', function (d) { return d.x0; })
-    .attr('y', function (d) { return d.y0})
-    .attr('width', function (d) { return d.x1 - d.x0; })
-    .attr('height', function (d) { return d.y1 - d.y0})
-    .style("fill", function(d) {
-        return color(d.data[month]);})
-        .style("opacity", function(d) {
-            return opacity(d.data[month])
-        });
-
-    
-// and to add the text labels
-svg2
-.selectAll("text")
-.data(treemap.leaves())
-.enter()
-.append("text")
-.attr("x", function(d){ return d.x0+20})    // +10 to adjust position (more right)
-.attr("y", function(d){ return d.y0+30})    // +20 to adjust position (lower)
-.text(function(d){ if(d.data.transportation_type != "" && d.data.transportation_type != undefined && d.data.transportation_type != null) return d.data.transportation_type +""+ d.data[month]+"%"})
-.attr("font-size", "18px")
-.attr("fill", "white")
-}
+        }}
+    }
