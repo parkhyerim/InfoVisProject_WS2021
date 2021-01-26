137fbef99d72d595f4b0175b763fc6761e703f54
laurauready <Lauramaria.graeber@yahoo.de>
2021-01-26 17:35:17 +0100

    working monthly calculation

--- src/scripts/circularBarplotView.js
+++ src/scripts/circularBarplotView.js
@@ -2,6 +2,7 @@ export function Displaydestinationdata(selectedMonth="03"){
     let destinationData = [];
     let month, day;
     var temp = [];
+
     //let monthparam = selectedMonth[0].substr((selectedMonth[0].indexOf("-")+1), 2);
 
     d3.csv('../src/data/googlemobilityreport.csv').then(function(data){
@@ -11,8 +12,9 @@ export function Displaydestinationdata(selectedMonth="03"){
                 destinationData.push(element);
             }
         });
+        console.log(destinationData)
 
-        const dataByRegion = destinationData.reduce((newdata, value) => {
+       const dataByRegion = destinationData.reduce((newdata, value) => {
             // Group initialization
             if (!newdata[value["sub_region_1"]]) {
                 newdata[value["sub_region_1"]] = [];
@@ -23,57 +25,36 @@ export function Displaydestinationdata(selectedMonth="03"){
 
             return newdata;
         }, {});
+       const newarray = [];
+       const counterarray = []
 
-        console.log(dataByRegion);
+       for (let i=0; i<destinationData.length; i++){
+           let currentmonth = destinationData[i].date;
 
-
-        console.log(destinationData[0]["17.02.2020"])
-
-        //calculate average value for every month
-        for (let m=1; m<13; m++){
-
-            // if clause to make the month value fitting to the formatting in the dataset
-            if (m<10) month="0"+m;
-            else month=m;
-
-            for (let i=0; i< dataByRegion.length; i++){
-                //since each month has a different number of days and the data has some gaps for two days we need to store the individual number of days each month in the variable div
-                let div = 0;
-
-                for (let d=1; d<32; d++){
-
-                    // if clause to make the day value fitting to the formatting in the dataset
-                    if (d<10) day="0"+d;
-                    else day=d;
-
-                    //format of the date stored in a variable
-                    let s = "2020."+month+"."+day;
-                    let currentmonth = dataByRegion[i][date];
-                    currentmonth = currentmonth.substring(3, 4);
-                    console.log(currentmonth)
-                    while (month > currentmonth){
-                        if (dataByRegion[i]["retail_and_recreation_percent_change_from_baseline"] !== undefined && dataByRegion["retail_and_recreation_percent_change_from_baseline"][s] !== "") {
-                            div++;
-
-                            if (dataByRegion[i][month]== null)
-                                dataByRegion[i][month]= dataByRegion[i]["retail_and_recreation_percent_change_from_baseline"];
-
-                            else {dataByRegion[i][month]= parseFloat(dataByRegion[i][month]) + parseFloat(dataByRegion[i]["retail_and_recreation_percent_change_from_baseline"]);}
-                        }
-
-                    }
-
-                }
-
-                dataByRegion[i][month]=(parseFloat(dataByRegion[i][month])/div).toFixed(2);
-            }
-        }
-        console.log(dataByRegion);
+               let dest = 'retail_and_recreation_percent_change_from_baseline'
+               currentmonth = currentmonth.substring(5, 7);
+                   if (newarray[destinationData[i].sub_region_1] == undefined) {
+                       newarray[destinationData[i].sub_region_1] = {}
+                       counterarray[destinationData[i].sub_region_1] = {}
+                   }
+                   if (newarray[destinationData[i].sub_region_1][currentmonth]!== undefined){
+                       newarray[destinationData[i].sub_region_1][currentmonth] += parseFloat(destinationData[i][dest])
+                       counterarray[destinationData[i].sub_region_1][currentmonth] +=1
+                   }
+                   else {
+                       newarray[destinationData[i].sub_region_1][currentmonth] = parseFloat(destinationData[i][dest])
+                       counterarray[destinationData[i].sub_region_1][currentmonth] =1
+                   }
+       }
 
 
-        //generate TreeChart from the provided Dateset
-      //  createTreeChart(mobilityData, monthparam)
+       for (var prop in newarray){
+           for (month in newarray[prop]){
+               newarray[prop][month] = (parseFloat(newarray[prop][month]) / parseFloat(counterarray[prop][month])).toFixed(2)
+           }
+       }
     });
+
 };
 
 Displaydestinationdata();
