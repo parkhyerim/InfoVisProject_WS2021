b8c36483b95232c92baa21744cb46a2737335aa0
Laura Gräber <lauragraber@MBP-von-Laura.fritz.box>
2021-02-01 15:32:58 +0100

    working stacking

--- src/scripts/circularBarplotView.js
+++ src/scripts/circularBarplotView.js
@@ -34,7 +34,7 @@ export function Displaydestinationdata(selectedMonth="03"){
            let currentmonth = destinationData[i].date;
            let retail = 'retail_and_recreation_percent_change_from_baseline'
            let grocpharma = 'grocery_and_pharmacy_percent_change_from_baseline'
-               currentmonth = currentmonth.substring(5, 7);
+               currentmonth = parseInt(currentmonth.substring(5, 7));
            if (newarray[currentmonth] == undefined) {
                newarray[currentmonth] = []
                counterarray[currentmonth] = []
@@ -68,12 +68,16 @@ export function Displaydestinationdata(selectedMonth="03"){
            }
        }
 
+
+
         const arraydata = Object.entries(newarray).map(element => {
+            console.log(element)
+
 
             return Object.values(element[1])
         });
+        console.log(arraydata)
 
-       console.log(arraydata[3])
 
         createCircularBarplot(arraydata[3], destinationData);
     });
@@ -81,6 +85,11 @@ export function Displaydestinationdata(selectedMonth="03"){
 };
 
 Displaydestinationdata();
+
+
+
+
+
 
 function createCircularBarplot(data, testdata){
 
@@ -107,18 +116,6 @@ function createCircularBarplot(data, testdata){
         .append("g")
         .attr("transform", "translate(" + width / 2 + "," + ( height/2+100 )+ ")"); // Add 100 on Y translation, cause upper bars are longer
 
-    var arc = d3.arc()     // imagine your doing a part of a donut plot
-        .innerRadius(innerRadius)
-        .outerRadius(function(d) {
-
-
-            return y(d.total);
-                })
-        .startAngle(function(d) { return x(d.state); })
-        .endAngle(function(d) { return x(d.state) + x.bandwidth(); })
-        .padAngle(0.01)
-        .padRadius(innerRadius)
-
     // X scale
     var x = d3.scaleBand()
         .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
@@ -126,35 +123,30 @@ function createCircularBarplot(data, testdata){
         .domain(data.map(function(d) {return d.state; }) );
 
 
-    ; // The domain of the X axis is the list of states.
-
     // Y scale
     var y = d3.scaleRadial()
         .range([innerRadius, outerRadius])   // Domain will be define later.
         .domain([-60, 60]); // Domain of Y is from 0 to the max seen in the data, CAN BE ADAPTED later!!!!
 
-    // set Z scale
-    var z = d3.scaleRadial()
-        .domain(categories)
-        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
 
-   // svg.append("g")
-     //   .call(yAxis);
 
-    svg.append("g")
-        .selectAll("g")
-        .data(d3.stack().keys(categories)(data))
-        .join("g")
-        .attr("fill", d => {
-                console.log(d)
-                z(d.key)
-        }
-          )
-        .selectAll("path")
-        .data(d => d)
-        .join("path")
-        .attr("d", arc);
-
+    var xAxis = g => g
+        .attr("text-anchor", "middle")
+        .call(g => g.selectAll("g")
+            .data(data)
+            .join("g")
+            .attr("transform", d => `
+          rotate(${((x(d.state) + x.bandwidth() / 2) * 180 / Math.PI - 90)})
+          translate(${innerRadius},0)
+        `)
+            .call(g => g.append("line")
+                .attr("x2", -5)
+                .attr("stroke", "#000"))
+            .call(g => g.append("text")
+                .attr("transform", d => (x(d.state) + x.bandwidth() / 2 + Math.PI / 2) % (2 * Math.PI) < Math.PI
+                    ? "rotate(90)translate(0,16)"
+                    : "rotate(-90)translate(0,-9)")
+                .text(d => d.state)))
 
     var yAxis = g => g
         .attr("text-anchor", "middle")
@@ -179,6 +171,48 @@ function createCircularBarplot(data, testdata){
                 .clone(true)
                 .attr("fill", "#000")
                 .attr("stroke", "none")))
+
+    // set Z scale
+    var z = d3.scaleRadial()
+        .domain(categories)
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
+
+    svg.append("g")
+        .selectAll("g")
+        .data(d3.stack().keys(categories)(data))
+        .join("g")
+        .attr("fill", d => {
+                console.log(d)
+            console.log(d.key)
+                z(d.key)
+            }
+        )
+        .selectAll("path")
+        .data((d) => {
+            console.log(d)
+            return d})
+        .join("path")
+        .attr("d", arc);
+
+    var arc = d3.arc()     // imagine your doing a part of a donut plot
+        .innerRadius(innerRadius)
+        .outerRadius(function(d) {
+
+
+            return y(d.total);
+                })
+        .startAngle(function(d) { return x(d.data.state); })
+        .endAngle(function(d) { return x(d.data.state) + x.bandwidth(); })
+        .padAngle(0.01)
+        .padRadius(innerRadius)
+
+    svg.append("g")
+        .call(xAxis);
+
+    svg.append("g")
+        .call(yAxis);
+
+
 
 
 
